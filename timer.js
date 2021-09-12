69ab74ad5819a7e4a5c0916613c490fee188c153
Vladyslav Starodubets <dp181191svn@gmail.com>
2021-09-12 15:12:52 +0200

    finish hw

---
+++ timer.js
@@ -0,0 +1,58 @@
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+        this.intervalID = null;
+        this.getRefs().btnStart.addEventListener("click", this.start.bind(this));
+        this.getRefs().btnStop.addEventListener("click", this.stop.bind(this));
+        this.getRefs().btnStop.disabled = true;
+    }
+    getDateNow() {
+        return Date.now();
+    }
+    getRefs() {
+        const container = document.querySelector(this.selector);
+        const daysRef = container.querySelector('[data-value="days"]');
+        const hoursRef = container.querySelector('[data-value="hours"]');
+        const minsRef = container.querySelector('[data-value="mins"]');
+        const secsRef = container.querySelector('[data-value="secs"]');
+        const btnStart = container.querySelector(".start");
+        const btnStop = container.querySelector(".stop");
+        return {daysRef, hoursRef, minsRef, secsRef, btnStart, btnStop};
+    }
+    updateTimer({ daysRef, hoursRef, minsRef, secsRef }) {
+        const time = this.targetDate - this.getDateNow();
+        daysRef.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        hoursRef.textContent = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        minsRef.textContent = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        secsRef.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    }
+    start() {
+        this.intervalId = setInterval(() => {
+            this.updateTimer(this.getRefs())
+        }, 1000);
+        this.getRefs().btnStart.disabled = true;
+        this.getRefs().btnStop.disabled = false;
+    }
+    stop() {
+        clearInterval(this.intervalId);
+        this.getRefs().btnStart.disabled = false;
+        this.getRefs().btnStop.disabled = true;
+    }
+    pad(value) {
+        return String(value).padStart(2, "0");
+    }
+}
+
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Sep 17, 2021'),
+});
+const timer2 = new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('Sep 25, 2021'),
+});
+console.log(timer);
+// timer.start();
+// timer2.start();
